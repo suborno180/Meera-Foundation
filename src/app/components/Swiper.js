f763da41de4a5f68d340be18702ea43fb427201d
suborno180 <suborno.dev@gmail.com>
2024-06-15 16:12:18 +0600

    update swiper

--- src/app/components/Swiper.js
+++ src/app/components/Swiper.js
@@ -1,11 +1,11 @@
-"use client";
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react"; // Import Swiper from swiper/react
-import Image from "next/image";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import { Autoplay, Pagination, Navigation } from "swiper"; // Import Swiper modules
+'use client';
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import Image from 'next/image';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 export default function App() {
   return (
@@ -30,28 +30,28 @@ export default function App() {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper h-[500px] w-screen max-md:h-[245px]"
         style={{
-          '--swiper-navigation-color': '#27AB61', // Replace with your color
+          '--swiper-navigation-color': '#27AB61',
         }}
       >
-        <SwiperSlide className="h-[500px] w-screen max-md:h-[240px]  ">
+        <SwiperSlide className="h-[500px] w-screen max-md:h-[240px]">
           <Image src={'/frontslide1.jpg'} alt="slideimg1" fill={true} />
         </SwiperSlide>
-        <SwiperSlide className="h-[500px] w-screen max-md:h-[240px] ">
+        <SwiperSlide className="h-[500px] w-screen max-md:h-[240px]">
           <Image src={'/frontslide2.png'} alt="slideimg2" fill={true} />
         </SwiperSlide>
-        <SwiperSlide className="h-[500px] w-screen max-md:h-[240px] ">
-          <Image src={'/firstslide4'} fill={true} alt="slideimg3" />
+        <SwiperSlide className="h-[500px] w-screen max-md:h-[240px]">
+          <Image src={'/firstslide4.jpg'} fill={true} alt="slideimg3" />
         </SwiperSlide>
-        <SwiperSlide className="h-[500px] w-screen max-md:h-[240px] ">
-          <Image src={'/frontslide5'} fill={true} alt="slideimg5" />
+        <SwiperSlide className="h-[500px] w-screen max-md:h-[240px]">
+          <Image src={'/frontslide5.jpg'} fill={true} alt="slideimg5" />
         </SwiperSlide>
-        <SwiperSlide className="h-[500px] w-screen max-md:h-[240px] ">
+        <SwiperSlide className="h-[500px] w-screen max-md:h-[240px]">
           <Image src={'/frontslide6.png'} fill={true} alt="slideimg6" />
         </SwiperSlide>
-        <SwiperSlide className="h-[500px] w-screen max-md:h-[240px] ">
+        <SwiperSlide className="h-[500px] w-screen max-md:h-[240px]">
           <Image src={'/frontslide8.png'} fill={true} alt="slideimg7" />
         </SwiperSlide>
-        <SwiperSlide className="h-[500px] w-screen max-md:h-[240px] ">
+        <SwiperSlide className="h-[500px] w-screen max-md:h-[240px]">
           <Image src={'/frontslide9.png'} fill={true} alt="slideimg8" />
         </SwiperSlide>
       </Swiper>
